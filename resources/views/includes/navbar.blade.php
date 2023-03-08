b3969bc89a9cbad7a1c430d9e1f6fe6073805156
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-08 23:48:58 +0100

    add new header and new admin.home

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,51 +1,63 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<nav class="navbar navbar-expand-md bg-black mb-4">
     <div class="container">
-        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-            <div class="logo_laravel">
-                Cardè
-            </div>
-            {{-- config('app.name', 'Laravel') --}}
+        <a class="navbar-brand text-uppercase text-white fw-bold p-2" href="{{ url('/') }}">
+            <h3 class="m-0">Cardè</h3>
         </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler p-2 bg-white" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon color-white"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                    <a class="nav-link text-uppercase text-white fw-bold" aria-current="page"
+                        href="{{ url('/') }}">Home</a>
+                </li>
+                {{-- @auth
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase text-white fw-bold" aria-current="page"
+                            href="{{ route('admin.projects.index') }}">Projects</a>
+                    </li>
+                @endauth --}}
+                <li class="nav-item">
+                    <a class="nav-link text-uppercase text-white fw-bold" aria-current="page"
+                        href="{{ route('admin.projects.index') }}">About Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.projects.index') }}">{{ __('Projects') }}</a>
+                    <a class="nav-link text-uppercase text-white fw-bold" aria-current="page"
+                        href="{{ route('admin.projects.index') }}">Contact Us</a>
                 </li>
             </ul>
 
-            <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link text-uppercase text-white fw-bold"
+                            href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                     @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link text-uppercase text-white fw-bold"
+                                href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-uppercase text-white fw-bold"
+                            href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('admin.home') }}">{{ __('Dashboard') }}</a>
-                            <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            <a class="dropdown-item text-uppercase fw-bold"
+                                href="{{ route('admin.projects.index') }}">{{ __('Project') }}</a>
+                            <a class="dropdown-item text-uppercase fw-bold"
+                                href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                            <a class="dropdown-item text-uppercase fw-bold" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
@@ -58,6 +70,10 @@
                     </li>
                 @endguest
             </ul>
+
+
         </div>
+
+
     </div>
 </nav>
