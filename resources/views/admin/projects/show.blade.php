445f09c12c6a660869c4d183cda38187a25bab75
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-08 17:52:59 +0100

    add btns in projects.show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -12,8 +12,16 @@
                     <p>{{ $project['description'] }}</p>
                     <a href="{{ $project['url'] }}" class="btn btn-primary"><i class="fa-brands fa-github me-1"></i>Vai al
                         GitHub</a>
-
                 </div>
+            </div>
+            <div class="d-flex justify-content-end">
+                <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" class="delete-form">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-sm btn-danger me-2">Elimina</button>
+                </form>
+                <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-sm btn-warning me-2">Modifica</a>
+                <a href="{{ route('admin.projects.index') }}" class="btn btn-sm btn-secondary">Indietro</a>
             </div>
 
         </div>
