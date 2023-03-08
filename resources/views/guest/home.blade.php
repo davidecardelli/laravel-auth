7c633c7caeace7c056637531669d0b78a5a81055
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-08 17:33:48 +0100

    add projects.index

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -2,6 +2,6 @@
 @section('content')
     <div class="container text-center">
         <h1>Home</h1>
-        <p>Home page del sito dove atterreranno tutti gli ospiti.</p>
+        <p>Home page del sito dove atterreranno tutti gli ospiti</p>
     </div>
 @endsection
